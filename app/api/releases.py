8e22f8c4e663cf46f0d8292b7c0f8aeabdb36670
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-02-16 14:41:46 +0000

    fix: ENV

--- app/api/releases.py
+++ app/api/releases.py
@@ -1,16 +1,18 @@
-from flask import Blueprint, jsonify, request, Response
 import json
 
-from app.data_access_object import t_release
+from flask import Blueprint, Response, jsonify, request
 
+from app.data_access_object import t_release
 from app.db.db import Release_DB
 
 bp_releases = Blueprint('bp_releases', __name__, url_prefix='/api')
 
 DEFAULT_LIMIT = 100
 
+
 def fix_encoding(releases):
     return Response(json.dumps(releases, ensure_ascii=False), content_type='application/json; charset=utf-8')
+
 
 @bp_releases.route('/releases')
 def t_releases():
@@ -22,16 +24,18 @@ def t_releases():
 
     return press_release
 
+
 @bp_releases.route('/all_releases')
 def all_releases():
     limit = DEFAULT_LIMIT
     if request.args.get('limit') is not None:
         limit = int(request.args.get('limit'))
-    
+
     release_db = Release_DB.get_instance()
     all_releases = fix_encoding(release_db.get_all(limit))
-    
+
     return all_releases
+
 
 @bp_releases.route('/search')
 def search():
@@ -48,8 +52,8 @@ def search():
     if request.args.get('pr_type') is not None:
         pr_type = request.args.get('pr_type')
         args.append(pr_type)
-        
+
     release_db = Release_DB.get_instance()
     results = fix_encoding(release_db.search(limit, *args))
-    
+
     return results
--- app/db/db.py
+++ app/db/db.py
@@ -1,43 +1,67 @@
-import mysql.connector
-import os, json
-from dotenv import load_dotenv
-load_dotenv()
+import json
+import os
 
-    
+import mysql.connector
+
+import settings
+
+
 class Release_DB(object):
     _instance = None
     _cnx = None
-    
+
     def __new__(cls, *args, **kwars):
         if not cls._instance:
             cls._instance = super(Release_DB, cls).__new__(cls)
         return cls._instance
-    
+
     def __init__(self):
-        self.table = 'test_table1'
+        self.table = 'releases'
         self.db_config = {
+<<<<<<< Updated upstream
             'user': os.getenv('MYSQL_USER'),
             'password': os.getenv('MYSQL_PASSWORD'),
             'database': os.getenv('MYSQL_DATABASE'),
             'host': os.getenv('MYSQL_HOST'),
             'port': os.getenv('MYSQL_PORT'),
+||||||| Stash base
+            'user': os.getenv('MYSQL_USER'),
+            'password': os.getenv('MYSQL_PASSWORD'),
+            'database': os.getenv('MYSQL_DATABASE'),
+            'host': 'hackathon2023-winter-digital-dragons-backend-mysql-1',
+            'port': 3306,
+=======
+            'user': settings.MYSQL_USER,
+            'password': settings.MYSQL_PASSWORD,
+            'database': settings.MYSQL_DATABASE,
+            'host': settings.MYSQL_HOST,
+            'port': settings.MYSQL_PORT,
+>>>>>>> Stashed changes
         }
         self._cnx = self.connect_to_mysql()
 
     def connect_to_mysql(self):
+<<<<<<< Updated upstream
         try:
             return mysql.connector.connect(**self.db_config)        
         except mysql.connector.Error as err:
             raise Exception(f"Failed to connect to MySQL: {err}")
     
+||||||| Stash base
+        return mysql.connector.connect(**self.db_config)
+    
+=======
+        return mysql.connector.connect(**self.db_config)
+
+>>>>>>> Stashed changes
     @classmethod
     def get_instance(cls):
         if cls._instance is None:
             cls()
         return cls._instance
-    
-    def to_dict(self, 
-                body, 
+
+    def to_dict(self,
+                body,
                 company_id,
                 company_name,
                 created_at,
@@ -47,47 +71,46 @@ class Release_DB(object):
                 main_image,
                 main_image_fastly,
                 pr_type,
-                release_id, 
-                sub_category_id, 
-                sub_category_name, 
-                subtitle, 
-                title, 
+                release_id,
+                sub_category_id,
+                sub_category_name,
+                subtitle,
+                title,
                 url
                 ):
-        
+
         release = {
-                'company_name': company_name,
-                'company_id': company_id,
-                'release_id': release_id,
-                'title': title,
-                'subtitle': subtitle,
-                'url': url,
-                'lead_paragraph': lead_paragraph,
-                'body': body,
-                'main_image': main_image,
-                'main_image_fastly': main_image_fastly,
-                'main_category_id': main_category_id,
-                'main_category_name': main_category_name,
-                'sub_category_id': sub_category_id,
-                'sub_category_name': sub_category_name,
-                'pr_type': pr_type,
-                'created_at': created_at
-            }
+            'company_name': company_name,
+            'company_id': company_id,
+            'release_id': release_id,
+            'title': title,
+            'subtitle': subtitle,
+            'url': url,
+            'lead_paragraph': lead_paragraph,
+            'body': body,
+            'main_image': main_image,
+            'main_image_fastly': main_image_fastly,
+            'main_category_id': main_category_id,
+            'main_category_name': main_category_name,
+            'sub_category_id': sub_category_id,
+            'sub_category_name': sub_category_name,
+            'pr_type': pr_type,
+            'created_at': created_at
+        }
         return release
 
-
     # 記事を全件取得
+
     def get_all(self, limit=100):
-        cursor =self._cnx.cursor()
+        cursor = self._cnx.cursor()
         query = f"SELECT * FROM {self.table} LIMIT {limit}"
         cursor.execute(query)
-        
+
         releases = [self.to_dict(*c) for c in cursor]
-        
+
         cursor.close()
         self._cnx.close()
         return releases
-
 
     def search(self, limit=100, main_category_id: int = None, sub_category_id: int = None, pr_type: str = None):
         criteria = ""
@@ -106,9 +129,25 @@ class Release_DB(object):
 
         cursor = self._cnx.cursor()
         cursor.execute(query)
+<<<<<<< Updated upstream
         results = [self.to_dict(*c) for c in cursor]
         
+||||||| Stash base
+        resulst = [self.to_dict(*c) for c in cursor]
+        
+=======
+        results = [self.to_dict(*c) for c in cursor]
+
+>>>>>>> Stashed changes
         cursor.close()
         self._cnx.close()
+<<<<<<< Updated upstream
         
         return results
+||||||| Stash base
+        
+        return results
+=======
+
+        return results
+>>>>>>> Stashed changes
