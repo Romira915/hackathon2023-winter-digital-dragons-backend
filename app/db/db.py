8d673209b915ea4ab352290ebcaf8b5b7196468d
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-02-16 10:36:04 +0000

    fix: py env

--- app/db/db.py
+++ app/db/db.py
@@ -1,33 +1,36 @@
-from flask import Flask, jsonify
+import json
+import os
+
 import mysql.connector
-import os, json
 from dotenv import load_dotenv
-load_dotenv()
+from flask import Flask, jsonify
 
-
-app = Flask(__name__)
+import settings
 
 db_config = {
-    'user': os.getenv('MYSQL_USER'),
-    'password': os.getenv('MYSQL_PASSWORD'),
-    'database': os.getenv('MYSQL_DATABASE'),
-    'host': 'hackathon2023-winter-digital-dragons-backend-mysql-1',
-    'port': 3306,
+    'user': settings.MYSQL_USER,
+    'password': settings.MYSQL_PASSWORD,
+    'database': settings.MYSQL_DATABASE,
+    'host': settings.MYSQL_HOST,
+    'port': settings.MYSQL_PORT,
 }
 
 table = 'test_table1'
+
 
 def connect_to_mysql():
     return mysql.connector.connect(**db_config)
 
 # 記事を全件取得
+
+
 def get_articles(limit=100):
     cnx = connect_to_mysql()
     cursor = cnx.cursor()
     query = f"SELECT * FROM {table} LIMIT {limit}"
     cursor.execute(query)
     articles = []
-    
+
     for (body, company_id, company_name, created_at, lead_paragraph, main_category_id, main_category_name, main_image, main_image_fastly, pr_type, release_id, sub_category_id, sub_category_name, subtitle, title, url) in cursor:
         article = {
             'company_name': company_name,
@@ -52,7 +55,6 @@ def get_articles(limit=100):
     cnx.close()
     return articles
 
-    
+
 if __name__ == "__main__":
     print((get_articles()))
-    
