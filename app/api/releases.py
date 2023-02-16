aaaca8c1c12c9ea7d88a2e24acc2d685c7b90ae0
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-02-16 09:08:31 +0000

    add api/search

--- app/api/releases.py
+++ app/api/releases.py
@@ -10,9 +10,32 @@ DEFAULT_LIMIT = 100
 @bp_releases.route('/releases')
 def t_releases():
     limit = DEFAULT_LIMIT
+
     if request.args.get('limit') is not None:
         limit = int(request.args.get('limit'))
 
     press_release = t_release.get_releases(limit)
 
     return jsonify(press_release)
+
+
+@bp_releases.route('/search')
+def search():
+    category_id = None
+    prefectures_id = None
+    pr_type = None
+    search_word = None
+
+    if request.args.get('category_id') is not None:
+        category_id = int(request.args.get('category_id'))
+
+    if request.args.get('prefectures_id') is not None:
+        prefectures_id = int(request.args.get('prefectures_id'))
+
+    if request.args.get('pr_type') is not None:
+        pr_type = request.args.get('pr_type')
+
+    if request.args.get('search_word') is not None:
+        search_word = request.args.get('search_word')
+
+    return "null"
