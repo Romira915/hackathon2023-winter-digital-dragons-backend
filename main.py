8bc72e6e26e933128e20f8fbcffa4c4d589f3aa4
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-02-16 07:27:01 +0000

    add: module dotenv flask-cors

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, Flask
+from flask_cors import CORS
 
+import settings
 from app.api.releases import bp_releases
 
 app = Flask(__name__)
+CORS(app, resources={r'/*': {
+    'origins': settings.FRONTEND_ORIGIN
+}})
 
 
 @app.route("/")
