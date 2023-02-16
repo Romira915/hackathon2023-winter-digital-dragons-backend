76d786d69eb3698274c7aa61f37a6ab34dd5b5e7
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-02-16 07:04:11 +0000

    rename

--- app/api/releases.py
+++ app/api/releases.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 
-from app.data_access_object import release
+from app.data_access_object import t_release
 
 bp_releases = Blueprint('bp_releases', __name__, url_prefix='/api')
 
@@ -8,11 +8,11 @@ DEFAULT_LIMIT = 100
 
 
 @bp_releases.route('/releases')
-def releases():
+def t_releases():
     limit = DEFAULT_LIMIT
     if request.args.get('limit') is not None:
         limit = int(request.args.get('limit'))
 
-    press_release = release.get_releases(limit)
+    press_release = t_release.get_releases(limit)
 
     return jsonify(press_release)
